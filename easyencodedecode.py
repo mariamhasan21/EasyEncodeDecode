444c6caf0ce7ffced4ab9c175d4a47f296b40a19
Mariam Hasan <86767403+mariamhasan21@users.noreply.github.com>
2024-10-21 11:35:31 +0600

    Easy Encode Decode

--- easyencodedecode.py
+++ easyencodedecode.py
@@ -1,14 +1,15 @@
+#Easy Encode Decode
 import random
 import string
-random_chars = ''.join(random.choices(string.ascii_letters + string.digits, k=3))
+random_chars = ''.join(random.choices(string.ascii_letters + string.digits, k=3)) #let the random.choices choose where to select characters from
 print("_______________________Welcome_______________________")
 running = True
 def enicode():
   if choice.upper() == "E":
     if len(code) == 3:
-      reversed_code = code[::-1]
+      reversed_code = code[::-1] #used string method for jumping from the end to get the last number at the beginning
       print(f"Encoded message: {reversed_code}")
-    elif len(code) >= 3:
+    elif len(code) >= 3: #if len of the code is less than 3, it doesn't add the random characters
       reversed_code = code[::-1]
       en_code = random_chars + reversed_code + random_chars
       print(f"Encoded message: {en_code}")
@@ -17,7 +18,7 @@ def enicode():
 
 def decode():
   if choice.upper() == "D":
-    if len(code) <= 3:
+    if len(code) <= 3: #since len of code is less or equal to 3, no random characters are joined. So, only the word is reversed
       reversed_code = code[::-1]
       print(reversed_code)
     elif len(code) >= 3:
@@ -26,10 +27,10 @@ def decode():
       print(f"Decoded message: {orignal}")
     else:
       print("Invalid choice")
-while running:
+while running: #while loop is used to keep the code running as long as the user don't quit
   choice = input("Please enter 'E' for Encoding And 'D' For decoding (Press 'q' to break): ")
   if choice.upper() == "Q":
-    running = False
+    running = False #with false statment, the program stops running
     break  
   code = (input("Enter the message: "))
   if choice.upper() =="E":
